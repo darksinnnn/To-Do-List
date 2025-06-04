8df24e064742c3833dcd062dc767af5b84017eef
Ashish Singh <73785073+darksinnnn@users.noreply.github.com>
2025-06-05 00:56:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnwPD-_qz-CrTVwVFPuiPHc15DQIemgto",
-  authDomain: "todolist-38612.firebaseapp.com",
-  projectId: "todolist-38612",
-  storageBucket: "todolist-38612.firebasestorage.app",
-  messagingSenderId: "819241324551",
-  appId: "1:819241324551:web:7247a155b3c9fa4c7d1aa2",
-  measurementId: "G-1X0D3TDHCY"
 };
 
 const app = initializeApp(firebaseConfig);
